2e3e447173898b5db0f58a319502bf440200c348
burgett40872 <burgett40872@students.apsb.org>
2026-08-26 19:26:03 +0000

    message

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,37 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
+createPlatform(0,700,100,10,"lime")
+createPlatform(300,600,100,10,"lime")
+createPlatform(400,500,150,10,"lime")
+createPlatform(300,400,100,10,"lime")
+createPlatform(600,400,100,10,"lime")
+createBadPlatform(0,730,1400,10)
+createPlatform(700,200,10,200,"lime")
+createPlatform(500,300,100,10,"lime")
+createPlatform(700,200,200,10,"lime")
+createPlatform(700,400,200,10,"lime")
+createBadPlatform(730,350,50,10)
+createFakePlatform(1100,500,100,10,"lime")
+createPlatform(1200,700,100,10,"lime")
+createPlatform(1100,300,10,200,"lime")
+createPlatform(1300,600,10,100,"lime")
 
     // TODO 3 - Create Collectables
-
+createCollectable("steve",650,350)
+createCollectable("database",730,250)
+createCollectable("grace",1250,650)
 
 
     
     // TODO 4 - Create Cannons
-
+createCannon("top",350,500)
+createCannon("top",500,500)
+createCannon("right",400,500)
 
     
     
